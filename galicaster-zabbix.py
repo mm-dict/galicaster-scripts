965c73ec71d8fd236689e8b27462e1bce82bf5e9
Kristof Keppens <kkeppens@gmail.com>
2013-05-13 09:59:15 +0200

    cleanup script

--- galicaster-zabbix.py
+++ galicaster-zabbix.py
@@ -24,7 +24,6 @@ def get_recording_state(options):
     response = get_galicaster_state(options)
     print response['is-recording']
 
-
 # Actions dict
 actions = {"hostname": get_hostname,
            "current_profile": get_profile,
@@ -50,21 +49,12 @@ def process_action(parser, options):
     actions.get(options.action)(options)
 
 
-def get_config():
-    print 'todo'
-
-
 def main():
 
     parser = OptionParser(usage="%prog [-c] [-a]", version="%prog 0.1")
     parser.add_option("-a", "--action", dest="action", nargs=1, type="choice",
                         choices=["hostname", "current_profile", "network", "is_recording"],
                         help="Options: hostname, current_profile, network and is_recording.")
-    parser.add_option("-c", "--config", dest="config", nargs=1, type="string",
-                      help="Provide a config file containing the matterhorn server details, "
-                      "this config file can be the galicaster config file. If a separate config "
-                      "file is choosen, make sure it's in the ini format and contains the same values "
-                      "as the galicaster config file (for opencast connection).")
     parser.add_option("--host", dest="host", default="localhost",
                       help="Ip address for the galicaster agent, default is localhost")
     parser.add_option("-p", "--port", dest="port", default="8080",
@@ -73,7 +63,7 @@ def main():
     (options, args) = parser.parse_args()
 
     if not(options.action):
-        parser.error("Incorrect number of arguments, please provide an action and config file.")
+        parser.error("Incorrect number of arguments, please provide an action.")
 
     process_action(parser, options)
 
