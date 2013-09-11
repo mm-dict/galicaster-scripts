9f010e948c612902b709ed94f64e22498fee4744
Kristof Keppens <kkeppens@gmail.com>
2013-09-11 14:30:13 +0200

    adding the status action

--- galicaster-zabbix.py
+++ galicaster-zabbix.py
@@ -1,8 +1,17 @@
+import sys
 from optparse import OptionParser
 import requests
+from requests import RequestException
 
 # Rest endpoint to get the galicaster state
 GALICASTER_STATE = "/state"
+
+
+def get_status(options):
+    if get_galicaster_state(options):
+        print "Online"
+    else:
+        print "Offline"
 
 
 def get_hostname(options):
@@ -25,18 +34,25 @@ def get_recording_state(options):
     print response['is-recording']
 
 # Actions dict
-actions = {"hostname": get_hostname,
-           "current_profile": get_profile,
-           "network": get_network,
-           "is_recording": get_recording_state,
+actions = { "status": get_status,
+            "hostname": get_hostname,
+            "current_profile": get_profile,
+            "network": get_network,
+            "is_recording": get_recording_state,
            }
 
 
 def get_galicaster_state(options):
     url = "http://" + options.host + ":" + options.port
-    r = requests.get(url + GALICASTER_STATE)
+    try:
+        r = requests.get(url + GALICASTER_STATE)
+    except RequestException:
+        print "Offline"
+        sys.exit(1)
+
     if r.status_code != requests.codes.ok:
         r.raise_for_status()
+        sys.exit(1)
     response = r.json()
     return response
 
@@ -53,7 +69,7 @@ def main():
 
     parser = OptionParser(usage="%prog [-c] [-a]", version="%prog 0.1")
     parser.add_option("-a", "--action", dest="action", nargs=1, type="choice",
-                        choices=["hostname", "current_profile", "network", "is_recording"],
+                        choices=["status", "hostname", "current_profile", "network", "is_recording"],
                         help="Options: hostname, current_profile, network and is_recording.")
     parser.add_option("--host", dest="host", default="localhost",
                       help="Ip address for the galicaster agent, default is localhost")
