cd22712fe60d7ffd5f321b75f9c490044c5c4236
Kristof <kkeppens@gmail.com>
2014-09-24 12:18:25 +0200

    specify galicaster startup script to avoid conflicts with pushpic plugin

--- check_galicaster.sh
+++ check_galicaster.sh
@@ -1,9 +1,8 @@
-#!/bin/bash 
-SERVICE='galicaster'
+#!/bin/bash
+SERVICE='run_galicaster.py'
 
 CHECK=`ps aux | grep -v grep | grep python | grep -c $SERVICE`
 
-if [ "${CHECK}" == '0' ]
-then
+if [ "${CHECK}" = '0' ]; then
     nohup /usr/bin/galicaster &
 fi
