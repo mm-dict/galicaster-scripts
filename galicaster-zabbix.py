4dedb953bf046d9cff35800e3d3e6cce9a15a952
Kristof Keppens <kkeppens@gmail.com>
2015-10-01 12:01:38 +0200

    add collectd action that returns the recording status in a format that collectd can use to gather statistics

--- galicaster-zabbix.py
+++ galicaster-zabbix.py
@@ -1,4 +1,5 @@
 import sys
+import socket
 from optparse import OptionParser
 import requests
 from requests import RequestException
@@ -33,12 +34,33 @@ def get_recording_state(options):
     response = get_galicaster_state(options)
     print response['is-recording']
 
+def get_collectd_stats(options):
+    url = "http://" + options.host + ":" + options.port
+    hostname = socket.gethostname()
+    try:
+        r = requests.get(url + GALICASTER_STATE, timeout=2)
+    except RequestException:
+        value = 0
+
+    if r.status_code != requests.codes.ok:
+        value = 0
+
+    response = r.json()
+
+    if response['is-recording']:
+        value = 1
+    else:
+        value = 0
+
+    print "PUTVAL " + hostname + "/galicaster/recording N:" + str(value)
+
 # Actions dict
 actions = { "status": get_status,
             "hostname": get_hostname,
             "current_profile": get_profile,
             "network": get_network,
             "is_recording": get_recording_state,
+            "collectd": get_collectd_stats,
            }
 
 
@@ -69,7 +91,7 @@ def main():
 
     parser = OptionParser(usage="%prog [-c] [-a]", version="%prog 0.1")
     parser.add_option("-a", "--action", dest="action", nargs=1, type="choice",
-                        choices=["status", "hostname", "current_profile", "network", "is_recording"],
+                        choices=["status", "hostname", "current_profile", "network", "is_recording", "collectd"],
                         help="Options: hostname, current_profile, network and is_recording.")
     parser.add_option("--host", dest="host", default="localhost",
                       help="Ip address for the galicaster agent, default is localhost")
